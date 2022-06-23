d29fc2373ed7292cfc47b28b4038581f9cde3122
wasim9695 <wasim10786akram@gmail.com>
2022-06-23 10:04:51 +0530

    first commits

---
+++ src/app/components/pages/home-page/home-page.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomePageRoutingModule } from './home-page-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HomePageRoutingModule
+  ]
+})
+export class HomePageModule { }
