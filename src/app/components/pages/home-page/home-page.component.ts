c458c080f2fa24841507c87b644d66408fa51702
wasim9695 <wasim10786akram@gmail.com>
2022-07-02 16:16:22 +0530

    file update on home page

--- src/app/components/pages/home-page/home-page.component.ts
+++ src/app/components/pages/home-page/home-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  detailPage(){
+    this.router.navigate(['/product-detail']);
+    //product-detail
   }
 
 }
