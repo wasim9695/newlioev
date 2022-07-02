218b212e6a35451345bcc9efc42578517dd92a7e
wasim9695 <wasim10786akram@gmail.com>
2022-07-02 16:39:06 +0530

    add js

--- src/app/components/common/footer-style-one/footer-style-one.component.ts
+++ src/app/components/common/footer-style-one/footer-style-one.component.ts
@@ -10,6 +10,14 @@ export class FooterStyleOneComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.loadJsFile("assets/js/custom.js");  
   }
+ 
+  public loadJsFile(url) {  
+    let node = document.createElement('script');  
+    node.src = url;  
+    node.type = 'text/javascript';  
+    document.getElementsByTagName('head')[0].appendChild(node);  
+  }  
 
 }
